bda63a989745d473aa6ced20816520b7d3cf2aed
vague <jari.matilainen@gmail.com>
2024-01-19 15:36:52 +0100

    clean up

--- lib/Paster/Controller/Paste.pm
+++ lib/Paster/Controller/Paste.pm
@@ -1,13 +1,15 @@
 package Paster::Controller::Paste;
-use Mojo::Base 'Mojolicious::Controller';
 
-sub paste {
-  my $self = shift;
-  my $config = $self->app->plugin('Config');
+use Mojo::Base 'Mojolicious::Controller', -signatures;
+use Mojo::URL;
+use Path::Tiny;
+
+sub paste ($c) {
+  my $config = $c->app->plugin('Config');
 
   my $get_paths = sub {
     my ($filename) = @_;
-    my $host_path = $config->{'host_path'} // "/tmp";
+    my $host_path = $config->{'host_path'};
 
     unless($filename) {
       my @chars = split '', 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -26,24 +28,24 @@ sub paste {
         }
       } while -e "$host_path/$filename";
     }
-    my $host_url = $config->{'host_url'} // "https://localhost/";
-    ("$host_path/$filename", "$host_url/$filename");
+    my $host_url = $config->{'host_url'};
+    (path($host_path, $filename), Mojo::URL->new($host_url)->path($filename));
   };
 
-  my $files = $self->req->every_upload('file');
+  my $files = $c->req->every_upload('file');
   if($files->[0] && length $files->[0]->filename > 0) {
     my @output;
     for my $ufile (@$files) {
       my ($fs, $url) = $get_paths->($ufile->filename);
       $ufile->move_to($fs);
       push @output, $url;
-      $self->app->log->info("Wrote paste as $fs, weblink is $url");
+      $c->app->log->info("Wrote paste as $fs, weblink is $url");
     }
 
-    $self->render(text => join("<br>", @output));
+    $c->render(text => join("<br>", @output));
   }
   else {
-    my $data = $self->param('paste');
+    my $data = $c->param('paste');
     my $res;
     my @paths;
 
@@ -57,8 +59,8 @@ sub paste {
       $res = $!;
     }
 
-    $self->app->log->info("Wrote paste as $fs, weblink is $url");
-    $self->render(text => $res // "$url\n");
+    $c->app->log->info("Wrote paste as $fs, weblink is $url");
+    $c->render(text => $res // "$url\n");
   }
 }
 
