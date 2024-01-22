98e315b9b5d26145b34549044990d8a7949996b7
vague666 <jari.matilainen@gmail.com>
2024-01-22 13:16:38 +0100

    Rewrite to prettier code

--- lib/Paster/Controller/Paste.pm
+++ lib/Paster/Controller/Paste.pm
@@ -2,67 +2,45 @@ package Paster::Controller::Paste;
 
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use Mojo::URL;
-use Path::Tiny;
+use Mojo::File qw(path tempfile);
+
+$File::Temp::KEEP_ALL = 1;
 
 sub paste ($c) {
   my $config = $c->app->plugin('Config');
 
-  my $get_paths = sub {
-    my ($filename) = @_;
+  my $get_paths = sub ($filename = undef) {
     my $host_path = $config->{'host_path'};
-
-    unless($filename) {
-      my @chars = split '', 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      my $i = 0;
-      my $namelength = 4;
-      do {
-        $i++;
-        if($i >= ($config->{max_iter} // 100)) {
-          $namelength += 1;
-          $i = 0;
-        }
-        $filename = '';
-        for(1 .. $namelength) {
-          my $r = int(rand(@chars));
-          $filename .= $chars[$r];
-        }
-      } while -e "$host_path/$filename";
-    }
     my $host_url = $config->{'host_url'};
-    (path($host_path, $filename), Mojo::URL->new($host_url)->path($filename));
+    $filename = tempfile(TEMPLATE => 'XXXXX',
+                                DIR => $host_path) unless $filename;
+
+    return (path($filename), Mojo::URL->new($host_url)->path($filename->basename));
   };
 
-  my $files = $c->req->every_upload('file');
-  if($files->[0] && length $files->[0]->filename > 0) {
-    my @output;
-    for my $ufile (@$files) {
-      my ($fs, $url) = $get_paths->($ufile->filename);
-      $ufile->move_to($fs);
-      push @output, $url;
-      $c->app->log->info("Wrote paste as $fs, weblink is $url");
-    }
-
-    $c->render(text => join("<br>", @output));
-  }
-  else {
-    my $data = $c->param('paste');
+  my @output;
+  if(my $data = $c->param('paste')) {
     my $res;
     my @paths;
 
     my ($fs, $url) = $get_paths->();
+    $c->app->log->debug("$url");
 
-    if(open my $fh, '>', $fs) {
-      print $fh $data or $res = $!;
-      close $fh if $fh;
-    }
-    else {
-      $res = $!;
-    }
-
-    $c->app->log->info("Wrote paste as $fs, weblink is $url");
-    $c->stash(url => $url);
-    $c->render('paste/output');
+    $fs->spew($data) if $fs;
+    push @output, $url;
   }
+  else {
+    for my $ufile ($c->req->every_upload('file')->@*) {
+      next unless $ufile && length($ufile->filename);
+
+      my ($fs, $url) = $get_paths->(Mojo::File->new($ufile->filename));
+      $ufile->move_to($fs);
+      push @output, $url;
+    }
+  }
+
+  $c->stash(url => shift @output);
+  $c->render('paste/output', rest => \@output);
 }
 
 1;
