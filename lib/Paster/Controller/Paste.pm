a2f839c81050cd46010a3f59dc683d72791c2a30
vague666 <jari.matilainen@gmail.com>
2024-02-21 12:56:56 +0100

    Remove commented-out code

--- lib/Paster/Controller/Paste.pm
+++ lib/Paster/Controller/Paste.pm
@@ -17,7 +17,6 @@ sub paste ($c) {
     $fs = tempfile(TEMPLATE => $config->{paste_template},
                    DIR => $host_path) unless $filename;
 
-    #my $path = Mojo::URL->new(($fs ? $fs : $filename)->basename)->url_escape;
     my $path = Mojo::URL->new(($fs ? $fs : $filename)->basename);
     return (path($fs // path($host_path, $filename)), Mojo::URL->new($host_url)->path($path));
   };
